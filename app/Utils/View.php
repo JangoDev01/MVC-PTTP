9b902ac1e0346ff8783b5e5507754ff53a09cfc2
JangoDev01 <miguelgabriel072001@gmail.com>
2024-11-05 15:01:30 +0300

    chamando o metodo render na home pra ver se esta tudo a funcionar conforme antes de deixar as coisas mais complexas

--- app/Utils/View.php
+++ app/Utils/View.php
@@ -11,7 +11,7 @@ class View{
      */
 
     private static function getContentView($view){
-        $file = __DIR__.'/../../resources/'.$view.'.html';
+        $file = __DIR__.'/../../resources/view/'.$view.'.html';
 
         return file_exists($file) ? file_get_contents($file) : '';
     }
--- app/controller/Pages/Home.php
+++ app/controller/Pages/Home.php
@@ -2,15 +2,17 @@
 
 namespace App\Controller\Pages;
 
+use App\Utils\View;
+
 class Home{
 
 
     /**
      * METODO RESPONSAVEL POR RETORNAR O CONTEUDO (view) DA NOSSA HOME
-     * @return [type]
+     * @return string
      *  */    
     public static function getHome(){
-        return "Ola Mundo";
+        return View::render('pages/home');   
     }
 
 }
