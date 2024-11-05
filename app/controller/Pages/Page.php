4a6d570a556eaecac31da1eed1657eb00c5efeb5
JangoDev01 <miguelgabriel072001@gmail.com>
2024-11-05 18:40:56 +0300

    umas mechidas basicas no bootstrap

--- app/controller/Pages/Page.php
+++ app/controller/Pages/Page.php
@@ -13,8 +13,8 @@ class Page{
      *  */    
     public static function getPage($title,$content){
         return View::render('pages/page', [
-            'title' => '$title',
-            'content' => '$content'
+            'title' => $title,
+            'content' => $content
         ]);   
     }
 
