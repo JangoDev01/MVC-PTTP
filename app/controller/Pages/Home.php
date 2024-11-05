1830a0ef4fe29a95ff2b48b0924eb390d9ca38c7
JangoDev01 <miguelgabriel072001@gmail.com>
2024-11-05 14:27:26 +0300

    criando a classe home responsavel por retornar o conteudo da home

---
+++ app/controller/Pages/Home.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller\Pages;
+
+class Home{
+
+
+    /**
+     * METODO RESPONSAVEL POR RETORNAR O CONTEUDO (view) DA NOSSA HOME
+     * @return [type]
+     *  */    
+    public static function getHome(){
+        return "Ola Mundo";
+    }
+
+}
