d31037412aede540f10178f7fc99a598fe861696
JangoDev01 <miguelgabriel072001@gmail.com>
2024-11-05 14:50:09 +0300

    criando os metodos getContentView que verifica se o arquivo da view existe para retornar o conteudo de uma view e o render que retorna o conteudo renderizado de uma view

---
+++ app/Utils/View.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Utils;
+
+class View{
+
+    /**
+     *  METODO RESPONSAVEL POR RETORNAR O CONTEUDO DE UMA VIEW
+     * @param string $view
+     * @param string
+     */
+
+    private static function getContentView($view){
+        $file = __DIR__.'/../../resources/'.$view.'.html';
+
+        return file_exists($file) ? file_get_contents($file) : '';
+    }
+
+
+    /**
+     * METODO RESPONSAVEL POR RETORNAR O CONTEUDO RENDERIZADO DE UMA VIEW
+     * @param string $view
+     * @param string
+     * 
+     */
+
+    public static function render($view){
+
+        // CONTEUDO DA VIEW
+        $contentView = self::getContentView($view);
+
+        //RETORNA O CONTEUDO RENDERIZADO
+        return $contentView;
+
+    }
+}
